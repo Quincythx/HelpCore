c2f1d45f8fbde9dfa37667065fdacc2b7fdd5aee
osedunme quincy <quincyosedunme@gmail.com>
2026-07-28 13:19:41 +0100

    Enable pagination globally (PageNumberPagination, 10 per page)

--- helpCore/settings.py
+++ helpCore/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
