748847e69909a380aadf9e43fc4804fd1d563b22
osedunme quincy <quincyosedunme@gmail.com>
2026-07-31 11:00:05 +0100

    Fix DATABASE_URL handling to avoid requiring local DB env vars in production

--- helpCore/settings.py
+++ helpCore/settings.py
@@ -79,12 +79,21 @@ WSGI_APPLICATION = 'helpCore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}",
-        conn_max_age=600,
-    )
-}
+if config('DATABASE_URL', default=None):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
+    }
 
 
 AUTH_USER_MODEL = 'accounts.User'
